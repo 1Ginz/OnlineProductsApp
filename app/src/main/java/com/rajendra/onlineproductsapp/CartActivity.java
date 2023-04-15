bc3f9a8ecb904d12489aa4da9ce5e068ee7e53a8
1Ginz <hunglq2908@gmail.com>
2023-04-15 14:12:23 +0700

    add activity

---
+++ app/src/main/java/com/rajendra/onlineproductsapp/CartActivity.java
@@ -0,0 +1,21 @@
+package com.rajendra.onlineproductsapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rajendra.onlineproductsapp.Helper.ManagementCart;
+
+public class CartActivity extends AppCompatActivity {
+
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewList;
+    private ManagementCart
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
---
+++ app/src/main/java/com/rajendra/onlineproductsapp/Helper/ManagementCart.java
@@ -0,0 +1,36 @@
+package com.rajendra.onlineproductsapp.Helper;
+
+import android.content.Context;
+
+import com.rajendra.onlineproductsapp.model.Products;
+
+import java.util.ArrayList;
+
+public class ManagementCart {
+    private Context context;
+    private TinyDB tinyDB;
+
+    public ManagementCart(Context context){
+        this.context = context;
+        this.tinyDB = new TinyDB(context);
+    }
+    public void insertItem(Products item){
+        ArrayList<Products> listItem = getListCart();
+        boolean existAlready = false;
+        int n = 0;
+        for(int i =0; i < listItem.size(); i++){
+            if(listItem.get(i).getProductid().equals(item.getProductid())){
+                existAlready = true;
+                n = i;
+                break;
+            }
+        }
+        if(existAlready == true){
+            listItem.get(n).set
+        }
+    }
+
+    public ArrayList<Products> getListCart(){
+        return tinyDB.getListObject("CartList");
+    }
+}
